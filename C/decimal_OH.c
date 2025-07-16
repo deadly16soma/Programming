dda868b17e750af0858c4d70877bab4fb642151c
somashekars <somashekars16k@gmail.com>
2025-07-16 18:21:26 +0530

    new_codes

---
+++ C/decimal_OH.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	int num;
+	printf("enter the number : ");
+	scanf("%d",&num);
+	printf("%o %x",num, num );
+        return 0;
+}	
---
+++ C/remainder.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	int num1, ans;
+	printf("enter any NUmber : ");
+	scanf("%d", &num1);
+	printf("%d", num1 % 3);
+	return 0;
+}
+
